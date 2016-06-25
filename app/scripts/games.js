4289cbbd930e9a0be4548c06bd58b436ed01a067
Heptet Group <heptetgroup@fastmail.fm>
2016-06-25 15:24:29 -0700

    successful ping
    Copied from Perforce
     Change: 175

--- app/scripts/games.js
+++ app/scripts/games.js
@@ -1,8 +1,23 @@
+var port = chrome.runtime.connect({name:'games'});
+
 chrome.runtime.onConnect.addListener(function(port) {
-    console.assert(port.name == "games");
+    console.assert(port.name == 'games');
+    console.log('received message');
     port.onMessage.addListener(function(msg) {
-	console.log(msg.event);
+	if(msg.event === 'pong')
+	{
+	    $('#logbox').append($('<span>pong<br></span>'));
+//	    document.write('pong<br>');
+	} else {
+	    console.log(msg.event);
+	}
     });
 });
-			      
 
+document.getElementById('pingbutton').addEventListener('click', doPing);
+
+
+function doPing()
+{
+    port.postMessage({event:'ping'});
+}
